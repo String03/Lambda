7b433a2920e38f72d0c98b18de3fd4ae1659eb71
String03 <40239185+String03@users.noreply.github.com>
2022-08-01 13:37:11 -0300

    Add files via upload

---
+++ Lambda/Lambda/Expresion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Expresion
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, int, decimal> H;
+            H = (msg, a, b) => Add(msg, a, b);
+            H("Mensaje",5, 5);
+
+            Console.ReadKey();
+        }
+
+        static decimal Add(string msg, int a, int b)
+        {
+            Console.WriteLine($"{msg} {a} {b}");
+            return a + b;
+        }
+    }
+}
